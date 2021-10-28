a3c75cbe9da3afdc24cfa75f79c889ac4787095b
kimvillanueva97 <kimvillanueva.it@gmail.com>
2021-10-28 17:17:24 +0800

    Left player health bar implementation

--- Assets/Scripts/DamageHandler.cs
+++ Assets/Scripts/DamageHandler.cs
@@ -1,14 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DamageHandler : MonoBehaviour
 {
     Animator animator;
+    [SerializeField] GameObject leftHealth;
+    Slider HealthBar;
+    float currentHealth;
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        HealthBar = leftHealth.GetComponent<Slider>();
+        currentHealth = HealthBar.value;
+
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -17,6 +24,9 @@ public class DamageHandler : MonoBehaviour
         if (other.gameObject.tag == "Weapon")
         {
             animator.SetTrigger("Damage");
+            currentHealth -= 10f;
+            if (currentHealth <= 0) currentHealth = 100;
+            HealthBar.SetValueWithoutNotify(currentHealth);
         }
     }
 }
