c6e3e8a0898cffd33fb26468e24b0976150347a5
kimvillanueva97 <kimvillanueva.it@gmail.com>
2021-10-29 00:20:45 +0800

    projectile animation tweak

--- Assets/Scripts/HadoukenHandler.cs
+++ Assets/Scripts/HadoukenHandler.cs
@@ -5,15 +5,43 @@ using UnityEngine;
 public class HadoukenHandler : MonoBehaviour
 {
     new Rigidbody2D rigidbody2D;
+    Animator animator;
+    bool hit = false;
     float thrust = 1f;
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void FixedUpdate()
     {
-        rigidbody2D.AddForce(transform.right * thrust);
+        if (!hit) rigidbody2D.AddForce(transform.right * thrust);
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (gameObject.tag == "PWeapon" && other.gameObject.tag == "Enemy")
+        {
+            Hit();
+        }
+        else if (gameObject.tag == "EWeapon" && other.gameObject.tag == "Player")
+        {
+            Hit();
+        }
+    }
+
+    void Hit()
+    {
+        rigidbody2D.constraints = RigidbodyConstraints2D.FreezePosition;
+        hit = true;
+        animator.SetBool("Hit", true);
+    }
+
+    void Death()
+    {
+        Destroy(gameObject);
     }
 }
